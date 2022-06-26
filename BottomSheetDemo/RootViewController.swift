03823f8776df642eff8497f6dfa617b00873cd70
Alex Rybachev <rybachev.as@gmail.com>
2022-06-26 19:50:58 +0400

    add button

--- BottomSheetDemo/RootViewController.swift
+++ BottomSheetDemo/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  BottomSheetDemo
 //
 //  Created by Aleksandr Rybachev on 26.06.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
